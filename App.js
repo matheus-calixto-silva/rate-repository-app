bf95406e214db7a5501c8c30c8a4a41843c5c30e
Matheus Calixto <matheuscalixto8@gmail.com>
2024-08-31 17:52:05 -0300

    fix(app): resolve issue with AppRegistry.registerComponent during app initialization

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+
+AppRegistry.registerComponent('main', () => App);
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
